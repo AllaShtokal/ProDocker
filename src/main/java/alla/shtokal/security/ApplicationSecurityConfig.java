7661aca07202a6846ec18907bb9a9b4e121401ca
Alla Shtokal <alla.shtokal@ttsw.com.pl>
2020-07-03 16:11:22 +0200

    statrted work on securit

--- src/main/java/alla/shtokal/security/ApplicationSecurityConfig.java
+++ src/main/java/alla/shtokal/security/ApplicationSecurityConfig.java
@@ -1,21 +1,22 @@
-//package alla.shtokal.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//}
+package alla.shtokal.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
