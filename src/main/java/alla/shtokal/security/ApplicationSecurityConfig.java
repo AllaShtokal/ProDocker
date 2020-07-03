79c85e9a082377290b7c4e99633cef166a78a1d9
Alla Shtokal <alla.shtokal@ttsw.com.pl>
2020-07-03 15:37:40 +0200

    statrted work on security

---
+++ src/main/java/alla/shtokal/security/ApplicationSecurityConfig.java
@@ -0,0 +1,21 @@
+//package alla.shtokal.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//}
